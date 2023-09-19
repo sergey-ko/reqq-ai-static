929d5f9d00d97edf8fa89f755cfa564fe5d6df5a
Sergey Kovalev <44134504+sergey-ko@users.noreply.github.com>
2023-09-19 16:47:01 +0600

    comment out unused

--- src/sections/_home/pricing-marketing.js
+++ src/sections/_home/pricing-marketing.js
@@ -1,77 +1,77 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Switch from '@mui/material/Switch';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+// import Stack from '@mui/material/Stack';
+// import Switch from '@mui/material/Switch';
+// import Container from '@mui/material/Container';
+// import Typography from '@mui/material/Typography';
 
-import PlanCard from './pricing-marketing-card';
+// import PlanCard from './pricing-marketing-card';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export default function PricingMarketing({ plans }) {
-  return (
-    <Container
-      sx={{
-        pt: 10,
-        pb: { xs: 5, md: 10 },
-      }}
-    >
-      <Stack
-        spacing={5}
-        alignItems={{ xs: 'center', md: 'flex-end' }}
-        direction={{ xs: 'column', md: 'row' }}
-        justifyContent={{ md: 'space-between' }}
-        sx={{ mb: { xs: 5, md: 10 } }}
-      >
-        <Stack
-          spacing={3}
-          sx={{
-            maxWidth: 480,
-            mx: { xs: 'auto', md: 'unset' },
-            textAlign: { xs: 'center', md: 'unset' },
-          }}
-        >
-          <Typography variant="overline" sx={{ color: 'text.disabled' }}>
-            Plans
-          </Typography>
+// export default function PricingMarketing({ plans }) {
+//   return (
+//     <Container
+//       sx={{
+//         pt: 10,
+//         pb: { xs: 5, md: 10 },
+//       }}
+//     >
+//       <Stack
+//         spacing={5}
+//         alignItems={{ xs: 'center', md: 'flex-end' }}
+//         direction={{ xs: 'column', md: 'row' }}
+//         justifyContent={{ md: 'space-between' }}
+//         sx={{ mb: { xs: 5, md: 10 } }}
+//       >
+//         <Stack
+//           spacing={3}
+//           sx={{
+//             maxWidth: 480,
+//             mx: { xs: 'auto', md: 'unset' },
+//             textAlign: { xs: 'center', md: 'unset' },
+//           }}
+//         >
+//           <Typography variant="overline" sx={{ color: 'text.disabled' }}>
+//             Plans
+//           </Typography>
 
-          <Typography variant="h2">Flexible approach to your needs</Typography>
+//           <Typography variant="h2">Flexible approach to your needs</Typography>
 
-          <Typography sx={{ color: 'text.secondary' }}>
-            Start small and grow
-          </Typography>
-        </Stack>
+//           <Typography sx={{ color: 'text.secondary' }}>
+//             Start small and grow
+//           </Typography>
+//         </Stack>
 
-        {/* <Stack direction="row" alignItems="center">
-          <Typography variant="overline">MONTHLY</Typography>
+//         {/* <Stack direction="row" alignItems="center">
+//           <Typography variant="overline">MONTHLY</Typography>
 
-          <Switch defaultChecked />
+//           <Switch defaultChecked />
 
-          <Typography variant="overline">YEARLY (save 10%)</Typography>
-        </Stack> */}
-      </Stack>
+//           <Typography variant="overline">YEARLY (save 10%)</Typography>
+//         </Stack> */}
+//       </Stack>
 
-      <Box
-        sx={{
-          gap: 4,
-          display: 'grid',
-          alignItems: 'center',
-          gridTemplateColumns: {
-            xs: 'repeat(1, 1fr)',
-            md: 'repeat(3, 1fr)',
-          },
-        }}
-      >
-        {plans.map((plan) => (
-          <PlanCard key={plan.license} plan={plan} />
-        ))}
-      </Box>
-    </Container>
-  );
-}
+//       <Box
+//         sx={{
+//           gap: 4,
+//           display: 'grid',
+//           alignItems: 'center',
+//           gridTemplateColumns: {
+//             xs: 'repeat(1, 1fr)',
+//             md: 'repeat(3, 1fr)',
+//           },
+//         }}
+//       >
+//         {plans.map((plan) => (
+//           <PlanCard key={plan.license} plan={plan} />
+//         ))}
+//       </Box>
+//     </Container>
+//   );
+// }
 
-PricingMarketing.propTypes = {
-  plans: PropTypes.array,
-};
+// PricingMarketing.propTypes = {
+//   plans: PropTypes.array,
+// };
